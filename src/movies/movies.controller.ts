641f14cbf41c770afad793155cae23979f20969f
ehddnr301 <dy950328@gmail.com>
2020-12-22 21:27:42 +0900

    feat : how to use body or query

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,8 +15,26 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `searching : ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string): string {
     return `This will return one movie with the id:${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData): string {
+    return movieData;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMoive: movieId,
+      ...updateData,
+    };
   }
 }
